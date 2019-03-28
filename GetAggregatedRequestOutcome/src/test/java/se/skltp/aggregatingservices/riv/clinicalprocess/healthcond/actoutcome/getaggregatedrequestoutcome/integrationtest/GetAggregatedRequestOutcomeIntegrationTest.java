d55e2fbe6c44f41b262aebe59bbfac19b329c29a
martul <mariia.tuliakova@soprasteria.com>
2019-03-28 12:19:51 +0100

    https://skl-tp.atlassian.net/browse/NTP-787 - aggTjänst använder nu aggCore med hsaCache och standardbehörighet och trädklättring - Tester för ny funktionalitet

--- GetAggregatedRequestOutcome/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getaggregatedrequestoutcome/integrationtest/GetAggregatedRequestOutcomeIntegrationTest.java
+++ GetAggregatedRequestOutcome/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getaggregatedrequestoutcome/integrationtest/GetAggregatedRequestOutcomeIntegrationTest.java
@@ -50,16 +50,19 @@ import org.soitoolkit.commons.mule.util.RecursiveResourceBundle;
 import riv.clinicalprocess.healthcond.actoutcome.getrequestoutcomeresponder.v4.GetRequestOutcomeResponseType;
 import riv.clinicalprocess.healthcond.actoutcome.v4.RequestOutcomeType;
 import se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.actoutcome.getaggregatedrequestoutcome.GetAggregatedRequestOutcomeMuleServer;
-import se.skltp.agp.cache.TakCacheBean;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusRecordType;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
 import se.skltp.agp.test.consumer.AbstractAggregateIntegrationTest;
-import se.skltp.agp.test.consumer.ExpectedTestData;
+import se.skltp.agp.test.consumer.TestData;
 import se.skltp.agp.test.producer.EngagemangsindexTestProducerLogger;
 import se.skltp.agp.test.producer.TestProducerLogger;
 
 public class GetAggregatedRequestOutcomeIntegrationTest extends AbstractAggregateIntegrationTest {
 
+	public GetAggregatedRequestOutcomeIntegrationTest() {
+		super(rb.getString("TAK_TJANSTEKONTRAKT"));
+	}
+	
 	private static final Logger log = LoggerFactory.getLogger(GetAggregatedRequestOutcomeIntegrationTest.class);
 
     private static final RecursiveResourceBundle rb = new RecursiveResourceBundle("GetAggregatedRequestOutcome-config");
@@ -81,12 +84,7 @@ public class GetAggregatedRequestOutcomeIntegrationTest extends AbstractAggregat
 			"teststub-services/service-producer-teststub-service.xml," +
 			"teststub-non-default-services/tak-teststub-service.xml";
 	}
-	
-	@Before
-	public void loadTakCache() throws Exception {
-	final TakCacheBean takCache = (TakCacheBean) muleContext.getRegistry().lookupObject("takCacheBean");
-	takCache.updateCache();
-	}
+
 	
 	/**
 	 * Perform a test that is expected to return zero hits
@@ -133,7 +131,7 @@ public class GetAggregatedRequestOutcomeIntegrationTest extends AbstractAggregat
     @Test
     public void test_ok_one_hit() {
 
-    	List<ProcessingStatusRecordType> statusList = doTest(TEST_RR_ID_ONE_HIT, 2, new ExpectedTestData(TEST_BO_ID_ONE_HIT, TEST_LOGICAL_ADDRESS_1));
+    	List<ProcessingStatusRecordType> statusList = doTest(TEST_RR_ID_ONE_HIT, 2, new TestData(TEST_BO_ID_ONE_HIT, TEST_LOGICAL_ADDRESS_1));
 
     	assertProcessingStatusDataFromSource(statusList.get(0), TEST_LOGICAL_ADDRESS_1);
     }
@@ -146,9 +144,9 @@ public class GetAggregatedRequestOutcomeIntegrationTest extends AbstractAggregat
 
     	// Setup call and verify the response, expect one booking from source #1, two from source #2 and a timeout from source #3
     	List<ProcessingStatusRecordType> statusList = doTest(TEST_RR_ID_MANY_HITS, 3,
-    		new ExpectedTestData(TEST_BO_ID_MANY_HITS_1, TEST_LOGICAL_ADDRESS_1),
-    		new ExpectedTestData(TEST_BO_ID_MANY_HITS_2, TEST_LOGICAL_ADDRESS_2),
-    		new ExpectedTestData(TEST_BO_ID_MANY_HITS_3, TEST_LOGICAL_ADDRESS_2));
+    		new TestData(TEST_BO_ID_MANY_HITS_1, TEST_LOGICAL_ADDRESS_1),
+    		new TestData(TEST_BO_ID_MANY_HITS_2, TEST_LOGICAL_ADDRESS_2),
+    		new TestData(TEST_BO_ID_MANY_HITS_3, TEST_LOGICAL_ADDRESS_2));
 
     	// Verify the Processing Status, expect ok from source system #1 and #2 but a timeout from #3
 		assertProcessingStatusDataFromSource(statusList.get(0), TEST_LOGICAL_ADDRESS_1);
@@ -176,7 +174,7 @@ public class GetAggregatedRequestOutcomeIntegrationTest extends AbstractAggregat
      * @param testData
      * @return
      */
-	private List<ProcessingStatusRecordType> doTest(String registeredResidentId, int expectedProcessingStatusSize, ExpectedTestData... testData) {
+	private List<ProcessingStatusRecordType> doTest(String registeredResidentId, int expectedProcessingStatusSize, TestData... testData) {
 		return doTest(registeredResidentId, SAMPLE_SENDER_ID, SAMPLE_ORIGINAL_CONSUMER_HSAID, SAMPLE_CORRELATION_ID, expectedProcessingStatusSize, testData);
     }
 
@@ -190,7 +188,7 @@ public class GetAggregatedRequestOutcomeIntegrationTest extends AbstractAggregat
      * @param testData
      * @return
      */
-	private List<ProcessingStatusRecordType> doTest(String registeredResidentId, String senderId, String originalConsumerHsaId, String correlationId, int expectedProcessingStatusSize, ExpectedTestData... testData) {
+	private List<ProcessingStatusRecordType> doTest(String registeredResidentId, String senderId, String originalConsumerHsaId, String correlationId, int expectedProcessingStatusSize, TestData... testData) {
 
 		// Setup and perform the call to the web service
 		GetAggregatedRequestOutcomeTestConsumer consumer = new GetAggregatedRequestOutcomeTestConsumer(DEFAULT_SERVICE_ADDRESS, senderId, originalConsumerHsaId, correlationId);
